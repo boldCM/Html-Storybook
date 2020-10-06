c8af25698f9801799c943203f92358119627c867
boldCM <carolin@cc-meier.de>
2020-10-06 14:44:03 +0200

    Remove Examples

--- src/components/transparentbutton/sharebutton.stories.js
+++
@@ -1,9 +0,0 @@
-import "./sharebutton.css";
-import shareButtonText from "./sharebutton.html";
-
-import heartButtonText from "./heartbutton.html";
-
-export default { title: "Components/transparentbutton" };
-
-export const withText = () => shareButtonText;
-export const withHeart = () => heartButtonText;
