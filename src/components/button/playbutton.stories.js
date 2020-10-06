353a8e992f7193fdcfb0b82850621dc94f94d13e
boldCM <carolin@cc-meier.de>
2020-10-06 14:42:21 +0200

    Fix only one File

---
+++ src/components/button/playbutton.stories.js
@@ -0,0 +1,6 @@
+import "./playbutton.css";
+import playButtonText from "./playbutton-text.html";
+
+export default { title: "Components/playbutton" };
+
+export const withText = () => playButtonText;
