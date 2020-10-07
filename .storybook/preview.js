565228b743d16a302db4676d057aba19065df553
boldCM <carolin@cc-meier.de>
2020-10-07 20:37:49 +0200

    Update centered Config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import { addDecorator, addParameters } from "@storybook/html"; // <- or your storybook framework
 import "@storybook/addon-console";
-import centered from "@storybook/addon-centered/html";
+// import centered from "@storybook/addon-centered/html";
 import "../src/global.css";
 
 // addDecorator(centered);
@@ -9,4 +9,5 @@ addParameters({
     config: {},
     options: {},
   },
+  layout: "fullscreen",
 });
--- src/components/button/sharebutton.stories.js
+++ src/components/button/sharebutton.stories.js
@@ -1,10 +1,11 @@
 import "./button.css";
+import centered from "@storybook/addon-centered/html";
 
 import shareButtonText from "./sharebutton.html";
 import playButtonText from "./playbutton-text.html";
 import heartButtonText from "./heartbutton.html";
 
-export default { title: "Components/Button" };
+export default { title: "Components/Button", decorators: [centered] };
 
 export const withPlay = () => playButtonText;
 export const withShare = () => shareButtonText;
