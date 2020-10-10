338601abd7c9299e281c3e75a87e88ceb8c358d0
boldCM <carolin@cc-meier.de>
2020-10-10 12:24:11 +0200

    Add more Buttons

---
+++ src/components/bottomNav/bottomNav.stories.js
@@ -0,0 +1,10 @@
+import "./bottomNav.css";
+
+import bottomNav from "./songs.html";
+import bottomNav from "./home.html";
+import bottomNav from "./settings.html";
+import bottomNav from "./none.html";
+
+export default { title: "Components/BottomNav" };
+
+export const BottomNav = () => bottomNav;
