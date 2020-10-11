20ca47dffe2dda3f0aabcee88d898bedc50b1e98
boldCM <carolin@cc-meier.de>
2020-10-11 21:13:32 +0200

    Add Seetings-Header

---
+++ src/components/settingsHeader/settingsHeader.stories.js
@@ -0,0 +1,6 @@
+import "./settingsHeader.css";
+import settingsHeader from "./settingsHeader.html";
+
+export default { title: "Components/SettingsHeader" };
+
+export const SettingsHeader = () => settingsHeader;
