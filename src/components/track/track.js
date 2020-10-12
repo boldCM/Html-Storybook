b6770152aba1a352f389c5ee43c085a9d1a0f419
boldCM <carolin@cc-meier.de>
2020-10-12 17:15:21 +0200

    Add Structure

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,33 +1,37 @@
 import "./track.css";
 
-import playElementImg from "../../assets/playActive.svg";
+import playImg from "../../assets/playActive.svg";
 import trackImg from "../../assets/michaelJackson.png";
 
 export function createTrackElement(title, artist) {
   const divContainer = document.createElement("div");
-  const divTrack = document.createElement("div");
-  const h2title = document.createElement("h2");
-  const h6artist = document.createElement("h6");
-  const buttonElement = document.createElement("button");
-  const playElement = document.createElement("img");
-  const trackElementImg = document.createElement("img");
-
-  h2title.innerText = title;
-  h6artist.innerText = artist;
-  playElement.src = playElementImg;
-  trackElementImg.src = trackImg;
-
   divContainer.className = "track";
-  trackElementImg.className = "albumIcon";
+
+  const divTrack = document.createElement("div");
   divTrack.className = "titleArtist";
+
+  const h2title = document.createElement("h2");
+  h2title.innerText = title;
+
+  const h6artist = document.createElement("h6");
+  h6artist.innerText = artist;
+
+  const buttonElement = document.createElement("button");
   buttonElement.className = "playIcon";
-
-  buttonElement.append(playElement);
-  divTrack.append(h2title, h6artist);
-  divContainer.append(trackElementImg, divTrack, buttonElement);
-
   buttonElement.onclick = function () {
     alert("click!");
   };
+
+  const playElementImg = document.createElement("img");
+  playElementImg.src = playImg;
+
+  const trackElementImg = document.createElement("img");
+  trackElementImg.src = trackImg;
+  trackElementImg.className = "albumIcon";
+
+  buttonElement.append(playElementImg);
+  divTrack.append(h2title, h6artist);
+  divContainer.append(trackElementImg, divTrack, buttonElement);
+
   return divContainer;
 }
