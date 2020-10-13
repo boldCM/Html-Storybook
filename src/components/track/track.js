659341770c6b0164ff22a063128dc941c0d1f84a
boldCM <carolin@cc-meier.de>
2020-10-13 17:08:29 +0200

    Add Music on Play

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,9 +1,9 @@
 import "./track.css";
 
 import playImg from "../../assets/playActive.svg";
-import trackImg from "../../assets/michaelJackson.png";
+// import trackImg from "../../assets/michaelJackson.png";
 
-export function createTrackElement(title, artist) {
+export function createTrackElement(track) {
   const divContainer = document.createElement("div");
   divContainer.className = "trackContainer";
 
@@ -11,14 +11,15 @@ export function createTrackElement(title, artist) {
   divText.className = "titleArtist";
 
   const h2title = document.createElement("h2");
-  h2title.innerText = title;
+  h2title.innerText = track.title;
 
   const h6artist = document.createElement("h6");
-  h6artist.innerText = artist;
+  h6artist.innerText = track.artist;
 
   const buttonElement = document.createElement("button");
   buttonElement.className = "playIcon";
   buttonElement.onclick = function () {
+    audioElement.play();
     alert("click!");
   };
 
@@ -27,9 +28,11 @@ export function createTrackElement(title, artist) {
   playElementImg.alt = "Icon of Play-Action button";
 
   const trackElementImg = document.createElement("img");
-  trackElementImg.src = trackImg;
+  trackElementImg.src = track.imgSrc;
   trackElementImg.alt = "Image of Album";
   trackElementImg.className = "albumIcon";
+
+  const audioElement = new Audio(track.audioSrc);
 
   buttonElement.append(playElementImg);
   divText.append(h2title, h6artist);
--- src/components/track/track.stories.js
+++ src/components/track/track.stories.js
@@ -2,7 +2,36 @@ import { createTrackElement } from "./track";
 
 export default { title: "Components/Track" };
 
-export const beatIt = () => createTrackElement("Beat it", "Michael Jackson");
-export const Thriller = () => createTrackElement("Thriller", "Michael Jackson");
+export const beatIt = () =>
+  createTrackElement({
+    track: "Beat it",
+    artist: "Michael Jackson",
+    imgSrc:
+      "https://www.thewowstyle.com/wp-content/uploads/2015/01/Michael-Jackson.png",
+    // audioSrc:
+  });
+export const Thriller = () =>
+  createTrackElement({
+    title: "Thriller",
+    artist: "Michael Jackson",
+    imgSrc:
+      "https://www.thewowstyle.com/wp-content/uploads/2015/01/Michael-Jackson.png",
+    // audioSrc:
+  });
 export const Billie = () =>
-  createTrackElement("Billie Jean", "Michael Jackson");
+  createTrackElement({
+    title: "Billie Jean",
+    artist: "Michael Jackson",
+    imgSrc:
+      "https://www.thewowstyle.com/wp-content/uploads/2015/01/Michael-Jackson.png",
+    // audioSrc:
+  });
+export const christmasAdventure = () =>
+  createTrackElement({
+    title: "A Christmas adventure",
+    artist: "TRG Banks",
+    imgSrc:
+      "https://files.freemusicarchive.org/storage-freemusicarchive-org/images/albums/TRG_Banks_-_TRG_Banks_Christmas_Album_-_20180915194354202.png?method=crop&width=290&height=290",
+    audioSrc:
+      "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/TRG_Banks/TRG_Banks_Christmas_Album/TRG_Banks_-_08_-_A_Christmas_adventure_Part_1.mp3",
+  });
