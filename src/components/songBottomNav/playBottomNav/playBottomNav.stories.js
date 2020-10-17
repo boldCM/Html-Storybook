0c91545ff028526630381e67a6b206b1b6eccf0c
boldCM <carolin@cc-meier.de>
2020-10-17 12:29:16 +0200

    Fix Naming Storybook

--- src/components/songBottomNav/playBottomNav/playBottomNav.stories.js
+++ src/components/songBottomNav/playBottomNav/playBottomNav.stories.js
@@ -1,5 +1,5 @@
 import { playBottomNav } from "./playBottomNav";
 
-export default { title: "Components/PlayBottomNav" };
+export default { title: "Components/PlaySettingsNav" };
 
 export const PlayBottomNav = () => playBottomNav();
