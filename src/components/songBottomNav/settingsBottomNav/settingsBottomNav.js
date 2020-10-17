5aee54514e592e03c76065fb9463390dd04109ae
boldCM <carolin@cc-meier.de>
2020-10-17 12:23:18 +0200

    Create Settings Bottom Nav Bar

---
+++ src/components/songBottomNav/settingsBottomNav/settingsBottomNav.js
@@ -0,0 +1,68 @@
+import "./settingsBottomNav.css";
+import { createElement } from "../../../utils/element";
+import playlist from "../playSongIcons/playlist.svg";
+import shuffle from "../playSongIcons/shuffle.svg";
+import repeat from "../playSongIcons/repeat.svg";
+import eq from "../playSongIcons/equalizer.svg";
+import favourites from "../../../assets/herz.svg";
+
+const playlistIcon = createElement("img", {
+  src: playlist,
+  alt: "play Icon",
+});
+const shuffleIcon = createElement("img", {
+  src: shuffle,
+  alt: "shuffle Icon",
+});
+const repeatIcon = createElement("img", {
+  src: repeat,
+  alt: "repeat Icon",
+});
+const equalizerIcon = createElement("img", {
+  src: eq,
+  alt: "equalizer Icon",
+});
+const favouritesIcon = createElement("img", {
+  src: favourites,
+  alt: "favourites Icon",
+});
+
+// Hilfsfunktion iconTitle:
+const createIconTitle = (iconText) => {
+  const iconTitle = createElement("span", {
+    innerText: iconText,
+  });
+  return iconTitle;
+};
+
+// Hier muss ich meine Hilfsfunktion drin aufrufen:
+const divPlaylist = createElement("div", {
+  className: "settingsBottomNav__playlist",
+  children: [playlistIcon, createIconTitle("Playlist")],
+});
+
+const divShuffle = createElement("div", {
+  className: "settingsBottomNav__shuffle",
+  children: [shuffleIcon, createIconTitle("Shuffle")],
+});
+const divRepeat = createElement("div", {
+  className: "settingsBottomNav__repeat",
+  children: [repeatIcon, createIconTitle("Repeat")],
+});
+const divEqualizer = createElement("div", {
+  className: "settingsBottomNav__eq",
+  children: [equalizerIcon, createIconTitle("EQ")],
+});
+const divFavourites = createElement("div", {
+  className: "settingsBottomNav__favourites",
+  children: [favouritesIcon, createIconTitle("Favourites")],
+});
+
+export const createSettingsBottomNav = () => {
+  const settingsBottomNav = createElement("div", {
+    className: "settingsBottomNav",
+    children: [divPlaylist, divShuffle, divRepeat, divEqualizer, divFavourites],
+  });
+
+  return settingsBottomNav;
+};
